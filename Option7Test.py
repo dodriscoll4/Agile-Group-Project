733d34b74cb31474a9bc595a828481cf75c32831
Mohammed Alhajri <145461669+mohalhjri2@users.noreply.github.com>
2023-11-21 14:09:27 +0000

    Upload test file for option menu 7

---
+++ Option7Test.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from CI_Code_Base import displaying_runners_who_have_not_gotten_podium
+
+class Option7Test(unittest.TestCase):
+    
+    def test_displaying_runners_who_have_not_gotten_podium(self):
+        races_location = ["Kinsale", "Blarney", "Newmarket", "Youghal", "Castletownbere"]
+        runners_name = ["Runner1", "Runner2", "Runner3", "Runner4", "Runner5"]
+        runners_id = ["ID1", "ID2", "ID3", "ID4", "ID5"]
+        expected_output = """Competitors who have not achieved a podium position in any race:
+=================================================================
+Runner1 (ID1)
+Runner2 (ID2)
+Runner3 (ID3)
+Runner4 (ID4)
+Runner5 (ID5)"""
+
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            displaying_runners_who_have_not_gotten_podium(races_location, runners_name, runners_id)
+
+        self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
