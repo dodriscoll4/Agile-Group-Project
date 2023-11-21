baf0dae6903dbb0a1dcf394f3d82bef79f710fe5
Mohammed Alhajri <145461669+mohalhjri2@users.noreply.github.com>
2023-11-21 14:08:36 +0000

    Upload test file for option menu 3

---
+++ Option3Test.py
@@ -0,0 +1,41 @@
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from CI_Code_Base import competitors_by_county
+
+
+class Option3Test(unittest.TestCase):
+
+    def test_competitors_by_county(self):
+        runners_name = ["Anna Fox", "Des Kelly", "Ann Cahill", "Joe Flynn", "Sally Fox", "Joe Shine", "Lisa Collins", "Sil Murphy", "Des Kelly"]
+        runners_id = ["CK-24", "CK-23", "KY-43", "CK-11", "KY-12", "TP-02", "WD-32", "LK-73", "WD-19"]
+
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            competitors_by_county(runners_name, runners_id)
+
+        expected_output = """Cork runners
+====================
+Anna Fox (CK-24)
+Des Kelly (CK-23)
+Joe Flynn (CK-11)
+Joe Shine (TP-02)
+
+Kerry runners
+====================
+Ann Cahill (KY-43)
+Sally Fox (KY-12)
+
+Limerick runners
+====================
+Sil Murphy (LK-73)
+
+Waterford runners
+====================
+Lisa Collins (WD-32)
+Des Kelly (WD-19)"""
+
+        self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
