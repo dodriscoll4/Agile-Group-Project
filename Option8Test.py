885450ca3877046134cc5d3d1db05eb2845db8d3
JamieRoche1997 <144720306+JamieRoche1997@users.noreply.github.com>
2023-11-28 00:59:00 +0000

    Update Option8Test.py

--- Option8Test.py
+++ Option8Test.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, mock_open
 from io import StringIO
 from Cl_Code_Base import main
 
@@ -7,7 +7,8 @@ from Cl_Code_Base import main
 class TestQuitOption(unittest.TestCase):
     @patch('sys.stdout', new_callable=StringIO)
     @patch('builtins.input', side_effect=['8'])
-    def test_quit_option(self, mock_input, mock_stdout):
+    @patch('builtins.open', new_callable=mock_open())
+    def test_quit_option(self, mock_open, mock_input, mock_stdout):
         main()
         output = mock_stdout.getvalue().strip()
         self.assertEqual(output, '')
